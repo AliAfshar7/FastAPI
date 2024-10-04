78d37bfb81b918970f2c629364a64f2f970ad7ef
AliAfshar7 <ali_a_78@yahoo.com>
2024-10-04 14:52:32 -0700

    send request from all domains

--- app/main.py
+++ app/main.py
@@ -8,40 +8,43 @@ from psycopg2.extras import RealDictCursor
 from sqlalchemy.orm import Session
 from . import models, schemas, utils
 from .database import engine, get_db
-from .routers import post, user, auth
-
-
-models.Base.metadata.create_all(bind=engine)
-
-try: 
-    conn = psycopg2.connect(host='192.168.192.219', database='fastapi', user='postgres', password='Kapitan 7',
-                            cursor_factory=RealDictCursor)
-    cursor = conn.cursor()
-    print("Dataset connection was successful")
-except Exception as error:
-    print("Dataset connection was failed")
-    print("Error:",error)
+from .routers import post, user, auth, vote
+from .config import settings
+from fastapi.middleware.cors import CORSMiddleware
 
 
 
-my_posts = []
+# models.Base.metadata.create_all(bind=engine)
 
-def find_post(id):
-    for post in my_posts:
-        if post['id'] == id:
-            return post
-        
-def find_index(id):
-    for i,post in enumerate(my_posts):
-        if post["id"] == id:
-            return i
+# try: 
+#     conn = psycopg2.connect(host='192.168.193.188', database='fastapi', user='postgres', password='Kapitan 7',
+#                             cursor_factory=RealDictCursor)
+#     cursor = conn.cursor()
+#     print("Dataset connection was successful")
+# except Exception as error:
+#     print("Dataset connection was failed")
+#     print("Error:",error)
         
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+@app.get("/")
+async def root():
+    return {'message':'hello world'}
+
 app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(auth.router)
-
+app.include_router(vote.router)
 
 
 
